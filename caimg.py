5d847abb1c406c7a896715eed1b229491c2a9dfe
Gehoon Chung <gehoon@users.noreply.github.com>
2018-10-27 08:48:12 +0900

    Update caimg.py

--- caimg.py
+++ caimg.py
@@ -2,8 +2,19 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
-# df = pd.read_excel('data/1_1.xlsx', sheetname='Sheet1')
+# read excel data
 df = pd.read_excel('data/1_1.xlsx')
 
-print("Column headings:")
-print(df.columns)
+# find timestamp
+expTime = df.iloc[0,0]
+
+# find startRow = Time (sec)
+startRow = max(df[df.iloc[:,0]=='Time (sec)'].index)
+df.columns = df.iloc[startRow,:]
+df = df.iloc[startRow+1:,:]
+
+# recalculate ratio
+
+
+df.iloc[0:3,:]
+df.columns
