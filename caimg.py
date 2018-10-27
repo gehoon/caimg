72cb80a7789b051c2248bbfc4920caf70aa6918f
Gehoon Chung <gehoon@users.noreply.github.com>
2018-10-27 17:10:42 +0900

    Update caimg.py

--- caimg.py
+++ caimg.py
@@ -14,11 +14,7 @@ expTime = df.iloc[0,0]
 # find startRow = Time (sec)
 startRow = max(df[df.iloc[:,0]=='Time (sec)'].index)
 df.columns = df.iloc[startRow,:]
-df = df.iloc[startRow+1:,:]
-
-# convert to all numerical dataframe
-dfDrug = df.loc[df.iloc[:,3].isna()]
-dfNum = df.loc[~df.iloc[:,3].isna()]
+df = df.iloc[startRow+1:,:].reset_index(drop=True)
 
 # recalculate ratio
 dfW1 = df.loc[:,df.columns.str.contains('W1 Avg')]
@@ -27,7 +23,21 @@ dfW2 = df.loc[:,df.columns.str.contains('W2 Avg')]
 dfW1.columns = dfW1.columns.str[0:-7]
 dfW2.columns = dfW2.columns.str[0:-7]
 
-dfRatio = dfW1 / dfW2
+dfRatio = pd.concat([df.loc[:,'Time (sec)'],dfW1 / dfW2], axis=1)
+
+# drug time
+drugs = df.loc[df.iloc[:,3].isna()].iloc[:,0:2]
+# dfNum = df.loc[~df.iloc[:,3].isna()]
+
+ctr1 = dfRatio.iloc[30:35,:].mean()
+
+df.iloc[drugs.index[0]-3:drugs.index[0]+4,:]
+df.iloc[125-3:125+4,:]
+
+drugRow1 = (df.loc[:,'Time (sec)'] - drug1).abs().argsort().iloc[0]
+drugRow1 = (df.loc[:,'Time (sec)'] - drugs).abs().argsort().iloc[0]
+min((df.loc[:,'Time (sec)'] - drug1).abs().index)
+
 
 # plotting
 fig, ax = plt.subplots()
