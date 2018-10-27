3c53f09a668c7c5991346e233bc209958fb1f874
Gehoon Chung <gehoon@users.noreply.github.com>
2018-10-27 14:26:29 +0900

    Update caimg.py

--- caimg.py
+++ caimg.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import re
+import numpy as np
+import matplotlib.pyplot as plt
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
@@ -13,8 +16,19 @@ startRow = max(df[df.iloc[:,0]=='Time (sec)'].index)
 df.columns = df.iloc[startRow,:]
 df = df.iloc[startRow+1:,:]
 
+# convert to all numerical dataframe
+dfDrug = df.loc[df.iloc[:,3].isna()]
+dfNum = df.loc[~df.iloc[:,3].isna()]
+
 # recalculate ratio
+dfW1 = df.loc[:,df.columns.str.contains('W1 Avg')]
+dfW2 = df.loc[:,df.columns.str.contains('W2 Avg')]
 
+dfW1.columns = dfW1.columns.str[0:-7]
+dfW2.columns = dfW2.columns.str[0:-7]
 
-df.iloc[0:3,:]
-df.columns
+dfRatio = dfW1 / dfW2
+
+# plotting
+fig, ax = plt.subplots()
+ax.plot(dfRatio)
